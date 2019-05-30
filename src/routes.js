e86d73d1c42fc826a81804c9dc69e1a1a1788fd2
gabriel <gdcardoso@gmail.com>
2019-05-30 20:47:58 -0300

    login completion

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,28 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
+const authMiddlewares = require('./app/middlewares/auth')
+const guestMiddlewares = require('./app/middlewares/guest')
+
 const UserController = require('./app/controller/UserController')
 const SessionController = require('./app/controller/SessionController')
 
-routes.get('/', SessionController.create)
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success')
+  res.locals.flashError = req.flash('error')
+
+  next()
+})
+
+routes.get('/', guestMiddlewares, SessionController.create)
 routes.post('/signin', SessionController.store)
 
-routes.get('/signup', UserController.create)
+routes.get('/signup', guestMiddlewares, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.use('/app', authMiddlewares)
+
+routes.get('/app/logout', SessionController.destroy)
 
 routes.get('/app/dashboard', (req, res) => {
   console.log(req.session.user)
